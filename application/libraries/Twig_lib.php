2e2f8f9bebae64ab26f050a4aaa99edd503f2736
hjue <haojue@gmail.com>
2014-07-31 13:58:39 +0800

    add twig library for ci

---
+++ application/libraries/Twig_lib.php
@@ -0,0 +1,46 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ * Created on 2013-10-25 by haojue
+ * Twig CI Library
+ *
+ */
+require_once APPPATH . 'third_party/Twig/Autoloader.php';
+
+class Twig_lib {
+
+    private $loader ;
+    private $twig;
+    private $_ci;
+
+    function __construct() {
+        Twig_Autoloader::register();
+        $this->_ci =& get_instance();
+        $this->loader = new Twig_Loader_Filesystem(FCPATH.'templates/'.$this->_ci->blog_config['template'].'/');
+        $this->twig = new Twig_Environment($this->loader, array(
+            'cache' => APPPATH.'cache',
+      'auto_reload' => true
+        ));
+    }
+
+    public function render($tpl,$data,$return = FALSE) {
+        $output = $this->twig->render($tpl,$data);
+
+        $this->_ci->output->append_output($output);
+        if ($return) {
+            return  $output;
+        }
+    } 
+  /**
+   * __call
+   * @param string $method
+   * @param array $args
+   * @throws Exception
+  */
+    public function __call($method,$args) {
+        $return = call_user_func_array(array($this->twig,$method),$args);
+        if ($return) {
+            return $return;
+        } 
+    }
+
+}
