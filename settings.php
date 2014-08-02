d7d96b9ac7af8de96435ffef8b8f233093a6f390
xieyu33333 <xieyu33333@gmail.com>
2014-08-02 14:33:50 +0800

    add tagside template

--- settings.php
+++ settings.php
@@ -3,5 +3,5 @@ $blog_config['title'] = 'Just Writing';
 $blog_config['intro'] = '这是介绍';
 $blog_config['author']='hjue';
 $blog_config['avatar']= 'http://tp4.sinaimg.cn/1663677355/180/5620649445/1';
-$blog_config['template'] = 'simple';
+$blog_config['template'] = 'tagside';
 $blog_config['comment'] = False;
