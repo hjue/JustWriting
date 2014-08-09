14c4c0fe865679efe9c1c0e7c7214704720d4c82
hjue <haojue@gmail.com>
2014-08-09 09:05:28 +0800

    设置rock为默认模板

--- settings.php
+++ settings.php
@@ -1,10 +1,10 @@
 <?php
 $blog_config['title'] = 'Just Writing';
 $blog_config['intro'] = 'Super Simple Blog Engine';
-$blog_config['author']='';
+$blog_config['author']='JustWriting';
 $blog_config['avatar']= 'https://raw.githubusercontent.com/hjue/JustWriting/develop/docs/logo.png';
-$blog_config['template'] = 'simple';
+$blog_config['template'] = 'rock';
 $blog_config['comment'] = True;
 $blog_config['duoshuo_short_name'] = 'justwriting';
-$blog_config['base_url'] = '';
-$blog_config['github'] = '';
+$blog_config['base_url'] = 'http://justwriting.sinaapp.com/';
+$blog_config['github'] = 'https://github.com/hjue/JustWriting';
