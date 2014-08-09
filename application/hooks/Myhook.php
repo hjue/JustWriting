a03d467657d5ed3e6964eeec16b8027a66b657c0
hjue <haojue@gmail.com>
2014-08-09 17:51:56 +0800

    remove debug info

--- application/hooks/Myhook.php
+++ application/hooks/Myhook.php
@@ -10,7 +10,6 @@ class Myhook
     ini_set('date.timezone', 'Asia/Shanghai');	    
     include(FCPATH.'/settings.php');    
     $CI = & get_instance();
-    print_r($blog_config);
     if($blog_config['base_url']==''){
       $blog_config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'];
     }    
