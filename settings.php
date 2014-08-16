ea5ac6538a37bdcce82926b7ac49bf8e72a3fdcd
hjue <haojue@gmail.com>
2014-08-16 10:43:06 +0800

    完善settings.php文档

--- settings.php
+++ settings.php
@@ -1,14 +1,31 @@
 <?php
+//博客标题
 $blog_config['title'] = 'Just Writing';
+//博客副标题
 $blog_config['intro'] = 'Super Simple Blog Engine';
+//博客作者
 $blog_config['author']='JustWriting';
+//博客头像
 $blog_config['avatar']= 'https://raw.githubusercontent.com/hjue/JustWriting/develop/docs/logo.png';
+//博客模板,模板在templates目录下，可自己定制
 $blog_config['template'] = 'rock';
+//是否开启评论,关闭设置为False
 $blog_config['comment'] = True;
+//多说评论系统的名字，可去多说申请 http://duoshuo.com/
 $blog_config['duoshuo_short_name'] = 'justwriting';
+//博客的域名如 http://justwriting.sinaapp.com,末尾不需要/
 $blog_config['base_url'] = '';
+//你的github地址，没有可以设置为空
 $blog_config['github'] = 'https://github.com/hjue/JustWriting';
 
+//是否开启通过Api发布文章功能，默认未开启
+$blog_config['api'] = False;
+//api_key的密码，请不要告诉其他人
+$blog_config['api_key'] = '';
+
+
+
+//有关dropgox的配置，详细配置方法见https://github.com/hjue/JustWriting
 $blog_config['dropbox']['key']= '';
 $blog_config['dropbox']['secret']= '';
 $blog_config['dropbox']['access_token']= '';
