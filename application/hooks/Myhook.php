d7fc2feb8243593730338331d0b76433c7eeedb8
hjue <haojue@gmail.com>
2014-09-28 21:26:31 +0800

    same schema with  page url

--- application/hooks/Myhook.php
+++ application/hooks/Myhook.php
@@ -11,7 +11,12 @@ class Myhook
     include(FCPATH.'/settings.php');    
     $CI = & get_instance();
     if($blog_config['base_url']==''){
-      $blog_config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'];
+      $schema = 'http';
+      if(isset($_SERVER['HTTPS']))
+      {
+        $schema = 'https';
+      }
+      $blog_config['base_url'] = $schema.'://'.$_SERVER['HTTP_HOST'];
     }    
     $CI->blog_config= $blog_config;
   }
