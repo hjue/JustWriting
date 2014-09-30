17bd30497b008ad9eb9ae3c8d542a17399d5725d
nhoway <n.hourmant@ovh.fr>
2014-09-30 22:56:33 +0200

    Create unit_test_lang.php
    Some datatypes traduced above sound weird, fell free to replace it.

---
+++ system/language/french/unit_test_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['ut_test_name']	= 'Nom du Test';
+$lang['ut_test_datatype']	= 'Type de Données du Test';
+$lang['ut_res_datatype']	= 'Type de Données Attendu';
+$lang['ut_result']	= 'Résultat';
+$lang['ut_undefined']	= 'Nom de test Indéfini';
+$lang['ut_file']	= 'Nom du Fichier';
+$lang['ut_line']	= 'Numéro de Ligne';
+$lang['ut_passed']	= 'Succès';
+$lang['ut_failed']	= 'Echec';
+$lang['ut_boolean']	= 'Booléen';
+$lang['ut_integer']	= 'Entier';
+$lang['ut_float']	= 'Flottant';
+$lang['ut_double']	= 'Double'; // can be the same as float
+$lang['ut_string']	= 'Chaîne';
+$lang['ut_array']	= 'Tableau';
+$lang['ut_object']	= 'Objet';
+$lang['ut_resource']	= 'Ressource';
+$lang['ut_null']	= 'Nul';
+$lang['ut_notes']	= 'Notes';
+
+/* End of file unit_test_lang.php */
+/* Location: ./system/language/french/unit_test_lang.php */
