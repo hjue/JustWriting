ad0927c61cdaa591553f35b091732a40e9137531
nhoway <n.hourmant@ovh.fr>
2014-09-30 22:51:54 +0200

    Create imglib_lang.php

---
+++ system/language/french/imglib_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['imglib_source_image_required'] = "Vous devez spécifier une image source dans vos préférences.";
+$lang['imglib_gd_required'] = "La librairie GD est nécessaire pour cette fonctionnalité.";
+$lang['imglib_gd_required_for_props'] = "Votre serveur doit supporter la librairie GD pour déterminer les propriétés de l'image.";
+$lang['imglib_unsupported_imagecreate'] = "Votre serveur ne supporte pas la fonction de la librairie GD utilisée pour traiter ce type d'image.";
+$lang['imglib_gif_not_supported'] = "Les images GIF ne sont pas toujours supportées en raison des restrictions de licence. Vous devriez utiliser le format PNG.";
+$lang['imglib_jpg_not_supported'] = "Les images JPG ne sont pas supportées.";
+$lang['imglib_png_not_supported'] = "Les images PNG ne sont pas supportées.";
+$lang['imglib_jpg_or_png_required'] = "Le protocole de redimensionnement spécifié dans vos préférences ne fonctionne qu'avec les formats JPEG ou PNG.";
+$lang['imglib_copy_error'] = "Une erreur s'est produite en remplaçant le fichier. Veuillez vérifier que le chemin spécifié accorde les droits en écriture.";
+$lang['imglib_rotate_unsupported'] = "La rotation d'image semble ne pas être prise en charge par votre serveur.";
+$lang['imglib_libpath_invalid'] = "Le chemin spécifié pour accéder à votre librairie d'images est incorrect. Veuillez corriger ce dernier dans vos préférences.";
+$lang['imglib_image_process_failed'] = "Traitement de l'image échoué. Veuillez vérifier que votre serveur supporte le protocole utilisé et que le chemin d'accès à votre image est correct.";
+$lang['imglib_rotation_angle_required'] = "Un angle de rotation est nécessaire pour faire pivoter l'image.";
+$lang['imglib_writing_failed_gif'] = "Image GIF.";
+$lang['imglib_invalid_path'] = "Le chemin d'accès à l'image est incorrect.";
+$lang['imglib_copy_failed'] = "Le processus de copie de l'image a échoué.";
+$lang['imglib_missing_font'] = "Impossible de trouver la police à utiliser.";
+$lang['imglib_save_failed'] = "Impossible d'enregistrer l'image. Veuillez vous assurer que l'image ainsi que son répertoire sont accessibles en écriture.";
+
+/* End of file imglib_lang.php */
+/* Location: ./system/language/french/imglib_lang.php */
