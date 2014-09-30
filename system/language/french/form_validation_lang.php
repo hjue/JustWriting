fe9d1d3b43c8194837d75ddb09129762cd190aed
nhoway <n.hourmant@ovh.fr>
2014-09-30 22:49:01 +0200

    Create form_validation_lang.php

---
+++ system/language/french/form_validation_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['required']	= "Le champ %s est requis.";
+$lang['isset']	= "Le champ %s requiert une valeur.";
+$lang['valid_email']	= "Le champ %s doit contenir une adresse courriel.";
+$lang['valid_emails']	= "Le champ %s ne doit contenir que des adresses courriel valides.";
+$lang['valid_url']	= "Le champ %s doit contenir une URL valide.";
+$lang['valid_ip']	= "Le champ %s doit contenir une IP valide.";
+$lang['min_length']	= "Le champ %s doit contenir au moins %s caractères.";
+$lang['max_length']	= "Le champ %s ne peut excéder %s caractères.";
+$lang['exact_length']	= "Le champ %s doit contenir exactement %s caractères.";
+$lang['alpha']	= "Le champ %s ne devrait contenir que des caractères alphabétiques.";
+$lang['alpha_numeric']	= "Le champ %s ne devrait contenir que des caractères alpha-numériques.";
+$lang['alpha_dash']	= "Le champ %s ne devrait contenir que des caractères alpha-numériques, des underscores, et tirets.";
+$lang['numeric']	= "Le champ %s ne doit contenir que des nombres.";
+$lang['is_numeric']	= "Le champ %s ne doit contenir que des caractères numériques.";
+$lang['integer']	= "Le champ %s doit contenir un nombre entier.";
+$lang['regex_match']	= "Le champ %s n'est pas au bon format.";
+$lang['matches']	= "Le champ %s ne correspond pas au champ %s.";
+$lang['is_unique'] = "Le champ %s doit contenir une valeur unique.";
+$lang['is_natural']	= "Le champ %s ne doit contenir que des nombres positifs.";
+$lang['is_natural_no_zero']	= "Le champ %s doit contenir un nombre strictement positif.";
+$lang['decimal']	= "Le champ %s doit contenir un nombre décimal.";
+$lang['less_than']	= "Le champ %s doit contenir un nombre inférieur à %s.";
+$lang['greater_than']	= "Le champ %s doit contenir un nombre supérieur à %s.";
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/french/form_validation_lang.php */
