e0dfe50085e3ff3c5ff3e866b5f8c294fedf61d0
nhoway <n.hourmant@ovh.fr>
2014-09-30 22:54:34 +0200

    Create profiler_lang.php

---
+++ system/language/french/profiler_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['profiler_database']	= "BASE DE DONNEES";
+$lang['profiler_controller_info'] = "CLASSE/METHODE";
+$lang['profiler_benchmarks']	= "INDICATEURS";
+$lang['profiler_queries']	= "REQUETES";
+$lang['profiler_get_data']	= "DONNEES GET";
+$lang['profiler_post_data']	= "DONNES POST";
+$lang['profiler_uri_string']	= "CHAINE URI";
+$lang['profiler_memory_usage']	= "UTILISATION MEMOIRE";
+$lang['profiler_config']	= "VARIABLES DE CONFIGURATION";
+$lang['profiler_session_data']	= "DONNES DE SESSION";
+$lang['profiler_headers']	= "ENTÊTE HTTP";
+$lang['profiler_no_db']	= "Le service de base de données est actuellement arrêté";
+$lang['profiler_no_queries']	= "Aucune requête n'a été effectuée";
+$lang['profiler_no_post']	= "Aucune donnée POST existante";
+$lang['profiler_no_get']	= "Aucune donnée GET existante";
+$lang['profiler_no_uri']	= "Aucune donnée URI existante";
+$lang['profiler_no_memory']	= "Utilisation mémoire non disponible";
+$lang['profiler_no_profiles']	= "Aucune donnée de profil disponible - toutes les sections ont été désactivées.";
+$lang['profiler_section_hide']	= "Masquer";
+$lang['profiler_section_show']	= "Afficher";
+
+/* End of file profiler_lang.php */
+/* Location: ./system/language/french/profiler_lang.php */
